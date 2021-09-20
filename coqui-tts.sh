876424e7b8b534e9efb7ea5ebf3bf4913fc1f008
naiba <hi@nai.ba>
2021-09-20 11:07:29 +0800

    🐛 fix github env in shell script

--- coqui-tts.sh
+++ coqui-tts.sh
@@ -7,6 +7,6 @@ git clone https://github.com/synesthesiam/coqui-docker.git && \
     cd coqui-docker && \
     git checkout $DOCKER_BUILDER_COMMIT_HASH && \
     cd coqui-tts && \
-    docker build -t ghcr.io/${{ github.repository_owner }}/coqui-tts --build-arg TTS_VERSION=$TTS_VERSION --build-arg TARGETARCH=$TARGETARCH --build-arg TARGETVARIANT=$TARGETVARIANT  . && \
-    docker push ghcr.io/${{ github.repository_owner }}/coqui-tts
+    docker build -t ghcr.io/$OWNER/coqui-tts --build-arg TTS_VERSION=$TTS_VERSION --build-arg TARGETARCH=$TARGETARCH --build-arg TARGETVARIANT=$TARGETVARIANT  . && \
+    docker push ghcr.io/$OWNER/coqui-tts
 
